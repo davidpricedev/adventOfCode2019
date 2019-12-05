3fbc680c1eb787cd817bcf3fddb7867dfc891233
David Price <david.price@revel-health.com>
2019-12-04 21:45:32 -0600

    day4 part2

--- src/main/kotlin/advent2019/day4/day4.kt
+++ src/main/kotlin/advent2019/day4/day4.kt
@@ -1,33 +1,48 @@
 package advent2019.day4
 
-import advent2019.day3.calculateClosestIntersection
-
-/*
-* Start with a filter function and see how far that goes
- */
-
 fun main() {
     runExamplesPart1()
-    runRange(264360, 746325)
+    runRangePart1(264360, 746325)
+    runExamplesPart2()
+    runRangePart2(264360, 746325)
 }
 
-fun runRange(start: Long, end: Long) =
-    println((start..end).filter(::filterNumber).count())
+fun runRangePart1(start: Long, end: Long) =
+    println((start..end).filter(::filterPart1).count())
+
+fun runRangePart2(start: Long, end: Long) =
+    println((start..end).filter(::filterPart2).count())
 
 fun runExamplesPart1() {
-    println("Examples:")
-    println(filterNumber(111111))
-    println(filterNumber(223450))
-    println(filterNumber(123789))
+    println("Examples Part1:")
+    println(filterPart1(111111))
+    println(filterPart1(223450))
+    println(filterPart1(123789))
     println("END Examples")
 }
 
-fun filterNumber(number: Long): Boolean {
-    val str = number.toString()
-    val digits = str.map { it.toInt() }
+fun runExamplesPart2() {
+    println("Examples Part2:")
+    println(filterPart2(112233))
+    println(filterPart2(123444))
+    println(filterPart2(111122))
+    println(filterPart2(112345))
+    println("END Examples")
+}
+
+fun filterPart1(number: Long): Boolean {
+    val digits = number.toString().map { it.toInt() }
     val uniqueDigits = digits.toSet()
     val is6Digits = digits.count() == 6
     val hasDouble = uniqueDigits.count() < 6
     val isIncreasing = digits.mapIndexed { i, it -> i == 0 || it >= digits[i - 1] }.reduce { acc, it -> acc && it }
     return is6Digits && hasDouble && isIncreasing
 }
+
+fun filterPart2(number: Long): Boolean {
+    val digits = number.toString().map { it.toInt() }
+    return filterPart1(number) && hasDigitAppearingExactlyTwice(digits)
+}
+
+fun hasDigitAppearingExactlyTwice(digits: List<Int>) =
+    digits.groupBy { it }.map { it.value.count() }.contains(2)
